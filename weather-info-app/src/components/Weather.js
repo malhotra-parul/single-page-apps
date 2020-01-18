e6c0a43bf427789b04a2300eb4f9e5affc089288
malhotra-parul <parulm.business@gmail.com>
2020-01-18 17:16:13 +0530

    Corrected format

--- weather-info-app/src/components/Weather.js
+++ weather-info-app/src/components/Weather.js
@@ -4,19 +4,19 @@ const Weather = props =>(
         <div className="weather__info">
             {
                 props.country && props.city && <p className="weather__key">Location : 
-                <span className="weather__value">{props.city}, {props.country}</span></p>
+                <span className="weather__value"> {props.city}, {props.country}</span></p>
                 }
             {
                 props.temp && <p className="weather__key">Temperature: 
-                <span className="weather__value">{props.temp}ºC</span></p>
+                <span className="weather__value"> {props.temp}ºC</span></p>
                 }
             {
                 props.humidity && <p className="weather__key">Humidity: 
-                <span className="weather__value">{props.humidity}%</span></p>
+                <span className="weather__value"> {props.humidity}%</span></p>
                 }
             {
                 props.desc && <p className="weather__key">Description: 
-                <span className="weather__value">{props.desc}</span></p>
+                <span className="weather__value"> {props.desc}</span></p>
                 }
             {
                 props.err && <p className="weather__error">{props.err}</p>
