c8c801fef8e6105ac14626bc658178bbbab98a3c
malhotra-parul <parulm.business@gmail.com>
2020-01-18 14:35:31 +0530

    Created a fully functional version of app

--- weather-info-app/src/App.js
+++ weather-info-app/src/App.js
@@ -8,13 +8,45 @@ const API_KEY = "60eceb8192ad8884a7c8759fb9ccf81a";
 
 
 class App extends React.Component{
+
+    state={
+        temperature: undefined,
+        humidity: undefined,
+        city: undefined,
+        country: undefined,
+        description: undefined,
+        err : undefined
+    }
     
     getWeather = async (e)=>{
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
         e.preventDefault();
+        try{
         let res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
-        console.log(res.data);
+        if(city && country){
+            console.log(res.data);
+            this.setState({
+                temperature: res.data.main.temp,
+                humidity: res.data.main.humidity,
+                city: res.data.name,
+                country: res.data.sys.country,
+                description: (res.data.weather[0].description).toUpperCase(),
+                err : ""
+            });
+        }
+    } catch(error){
+        console.log(error);
+        this.setState({
+            temperature: undefined,
+            humidity: undefined,
+            city: undefined,
+            country: undefined,
+            description: undefined,
+            err : "Please enter value for City and Country."
+        });
+    }
+
     };
 
     render(){
@@ -22,7 +54,12 @@ class App extends React.Component{
             <div>
                 <Titles />
                 <Forms getW={this.getWeather}/>
-                <Weather />
+                <Weather temp={this.state.temperature} 
+                         humidity={this.state.humidity}
+                         city={this.state.city}
+                         country={this.state.country}
+                         desc={this.state.description}
+                         err={this.state.err}/>
             </div>
         );
     }
--- weather-info-app/src/components/Weather.js
+++ weather-info-app/src/components/Weather.js
@@ -4,8 +4,12 @@ class Weather extends React.Component{
 render(){
     return(
         <div>
-                <h2>Weather Component</h2>
-            </div>
+            {this.props.country && this.props.city && <p>Location : {this.props.city}, {this.props.country}</p>}
+            {this.props.temp && <p>Temperature: {this.props.temp}ºC</p>}
+            {this.props.humidity && <p>Humidity: {this.props.humidity}%</p>}
+            {this.props.desc && <p>Description: {this.props.desc}</p>}
+            {this.props.err && <p>{this.props.err}</p>}
+        </div>
         );
     }
 }
