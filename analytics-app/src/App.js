ca49e13b5de9d109f6ec332db9a780e968c7411f
malhotra-parul <parulm.business@gmail.com>
2020-02-24 10:44:12 +0530

    Reconstructing youtube clone

---
+++ analytics-app/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+        <div className="App">
+            <h1>Hello world!</h1>
+        </div>
+        
+        )
+    }
+}
+
+export default App;
