e2bebbf9969775b64f722f9c5faf317d53032e8b
malhotra-parul <parulm.business@gmail.com>
2020-02-24 16:32:50 +0530

    added Spinner Loading Component

--- analytics-app/src/components/users/Users.js
+++ analytics-app/src/components/users/Users.js
@@ -1,20 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinners from "../layouts/Spinner";
 
-class Users extends Component{
-
-    render(){
-        return(
-            <div style={userStyle}>
-                {
-                    this.props.users.map(user => (
-
-                    <UserItem key={user.id} user={user}/>
-                    ))
-                }
-            </div>
-        );
-    }
+const Users = ({users, loading})=>{
+        if(loading){
+            return <Spinners />;
+        }else{
+            return(
+                <div style={userStyle}>
+                    {
+                        users.map(user => (
+    
+                        <UserItem key={user.id} user={user}/>
+                        ))
+                    }
+                </div>
+            );
+        }
 }
 
 const userStyle = {
