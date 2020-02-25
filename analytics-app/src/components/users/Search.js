74e05b356307bf463da345d9324c510c8e7b38ce
malhotra-parul <parulm.business@gmail.com>
2020-02-25 11:49:59 +0530

    Adding state for text to Search component

--- analytics-app/src/components/users/Search.js
+++ analytics-app/src/components/users/Search.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 class Search extends React.Component{
+    state={
+        text : ""
+    }
     render(){
         return(
             <div>
                 <form className="form">
-                    <input type="text" name="text" placeholder="Search for users..." />
+                    <input type="text" 
+                    name="text" 
+                    placeholder="Search for users..." 
+                    value={this.state.text}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 </form>
             </div>
