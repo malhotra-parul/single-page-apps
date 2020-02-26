924c98d037108f8994c05dd17520f41e20573436
malhotra-parul <parulm.business@gmail.com>
2020-02-26 22:29:42 +0530

    Imported Alert into App

--- analytics-app/src/App.js
+++ analytics-app/src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/layouts/NavBar";
 import axios from "axios";
 import Users from "./components/users/Users";
 import Search from "../src/components/users/Search";
+import Alert from "../src/components/layouts/Alert";
 const client_id = process.env.REACT_APP_CLIENT_ID;
 const secret_key = process.env.REACT_APP_CLIENT_SECRET;
 
@@ -29,7 +30,7 @@ class App extends Component{
         this.setState({loading: true});
         const res = await axios
             .get(`https://api.github.com/users/${text}?client_id=${client_id}&client_secret=${secret_key}`);
-        console.log(text, res.data);
+        console.log(text, res.data  );
         this.setState({
             loading: false,
             users: res.data
@@ -58,6 +59,7 @@ class App extends Component{
             <div className="App">
                     <NavBar />
                     <div className="container">
+                    <Alert alert={this.state.alert}/>
                     <Search searchUsers={this.searchUsers} 
                             clearUsers={this.clearUsers}
                             showClear={this.state.users.length>1 ? true : false}
