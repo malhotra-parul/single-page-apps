9d22d6ebbf93a4df3f461833d5c935ac0a130737
malhotra-parul <parulm.business@gmail.com>
2020-02-29 18:27:57 +0530

    Setting state on clicking any video item using onVideoSelect method

--- youtube-clone-app/src/components/App.js
+++ youtube-clone-app/src/components/App.js
@@ -23,7 +23,7 @@ class App extends React.Component{
     };
 
     onVideoSelect = (video)=>{
-        console.log("From the app", video);
+        this.setState({selectedVideo: video});
     };
     
     render(){
