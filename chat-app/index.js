29fc97d7f5b4684ddbd33c59da2a11fd3c8139c8
malhotra-parul <parulm.business@gmail.com>
2020-03-05 00:12:36 +0530

    Created a form in html to be displayed

--- chat-app/index.js
+++ chat-app/index.js
@@ -2,9 +2,9 @@ var app = require("express")();
 var http = require("http").createServer(app);
 
 app.get("/", (req, res)=>{
-    res.send("<h1>Hello World! </h1>");
+    res.sendFile(__dirname + "/index.html");
 });
 
 http.listen(3000, ()=>{
     console.log("Listening on port 3000");
-})
+});
