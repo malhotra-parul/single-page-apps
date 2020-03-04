d2ce13c68d7d671ac82dca5b281c5a92ee3e6000
malhotra-parul <parulm.business@gmail.com>
2020-03-04 23:56:59 +0530

    Setting up the initial application

---
+++ chat-app/index.js
@@ -0,0 +1,10 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+
+app.get("/", (req, res)=>{
+    res.send("<h1>Hello World! </h1>");
+});
+
+http.listen(3000, ()=>{
+    console.log("Listening on port 3000");
+})
