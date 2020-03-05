0e0db651eab5adccc3b63ef7b97b8973b1ed1df2
malhotra-parul <parulm.business@gmail.com>
2020-03-05 22:52:07 +0530

    Created a basic express app, index.html to serve static content and css file

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,18 +1,9 @@
-var app = require("express")();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http);
-
-
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/index.html");
-});
-
-io.on("connection", (socket)=>{
-    socket.on("chat message", (msg)=>{
-        console.log("Message : " +msg);
-    });
-});
-
-http.listen(3000, ()=>{
+var express = require("express");
+//app setup
+var app = express();
+//create a server
+var server = app.listen(3000, ()=>{
     console.log("Listening on port 3000");
-});
+}) ;
+//serve static files on client 
+app.use(express.static("public"));
