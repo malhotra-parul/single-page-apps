044fb1df9144a687f8cea4a65630f982c7a78cfc
malhotra-parul <parulm.business@gmail.com>
2020-03-05 23:14:19 +0530

    Made server side socket connection

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var socket = require("socket.io");
+
 //app setup
 var app = express();
 //create a server
@@ -7,3 +9,9 @@ var server = app.listen(3000, ()=>{
 }) ;
 //serve static files on client 
 app.use(express.static("public"));
+
+//socket server side setup
+var io = socket(server); //A socket now sits on the server listening to any connections.
+io.on("connection", (socket)=>{
+    console.log("Made socket connection: " + socket);
+});//io listens to an event - connection. Once connection is made, the callback is fired.
