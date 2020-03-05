ef4f03683cfc709a82637749d89eff6b5ea25078
malhotra-parul <parulm.business@gmail.com>
2020-03-05 21:30:45 +0530

    Listening to connection event for incoming sockets

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,9 +1,15 @@
 var app = require("express")();
 var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/index.html");
 });
+
+io.on("connection", (socket)=>{
+    console.log("A user connected!");
+})
 
 http.listen(3000, ()=>{
     console.log("Listening on port 3000");
