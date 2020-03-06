0db1969bccb3b34f6ce4bcc40eae429c392d4ef2
malhotra-parul <parulm.business@gmail.com>
2020-03-06 13:02:06 +0530

    Created variables to query DOM in chat.js

--- chat-app/public/chat.js
+++ chat-app/public/chat.js
@@ -1,2 +1,8 @@
 //Make a socket connection on client side
 var socket = io.connect("http://localhost:3000");
+
+var message = document.getElementById("message");
+var output = document.getElementById("output");
+var handle = document.getElementById("handle");
+var button = document.getElementById("send");
+
