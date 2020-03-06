54bb5af69cc7f88da568174388e8705abc5215f2
malhotra-parul <parulm.business@gmail.com>
2020-03-06 23:38:51 +0530

    Implemented a chat app

--- chat-app/index.js
+++ chat-app/index.js
@@ -4,7 +4,7 @@ var socket = require("socket.io");
 //app setup
 var app = express();
 //create a server
-var server = app.listen(3000, ()=>{
+var server = app.listen(4000, ()=>{
     console.log("Listening on port 3000");
 }) ;
 //serve static files on client 
@@ -14,4 +14,15 @@ app.use(express.static("public"));
 var io = socket(server); //A socket now sits on the server listening to any connections.
 io.on("connection", (socket)=>{
     console.log("Made socket connection: ", socket.id);
+
+    //handle data received from client
+    socket.on("chat", (data)=>{
+        //send it to all clients for live reloading
+        io.sockets.emit("chat", data);
+    });
+    socket.on("typing", (data)=>{
+        socket.broadcast.emit("typing", data);
+    })
 });//io listens to an event - connection. Once connection is made, the callback is fired.
+
+    
--- chat-app/public/chat.js
+++ chat-app/public/chat.js
@@ -1,8 +1,35 @@
 //Make a socket connection on client side
-var socket = io.connect("http://localhost:3000");
+var socket = io.connect("http://localhost:4000");
 
 var message = document.getElementById("message");
 var output = document.getElementById("output");
 var handle = document.getElementById("handle");
 var button = document.getElementById("send");
+var feedback = document.getElementById("feedback");
+
+//button will listen to click event and once received, it will emit a chat message
+// with the object being passed to server
+
+button.addEventListener("click", ()=>{
+    socket.emit("chat", {
+        message: message.value,
+        handle: handle.value
+    })
+});
+
+//emit event for typing
+message.addEventListener("keypress", ()=>{
+    socket.emit("typing",handle.value)
+});
+
+//listen for chat event
+socket.on("chat", (data)=>{
+    feedback.innerHTML = "";
+    output.innerHTML += "<p><strong>" + data.handle + ":</strong>" + data.message + "</p>";
+
+});
+
+socket.on("typing", (data)=>{
+    feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>";
+});
 
