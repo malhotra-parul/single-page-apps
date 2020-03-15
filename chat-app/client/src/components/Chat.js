8dd7e26b48140ae5fa2d912f8dc1c24cd5b85774
malhotra-parul <parulm.business@gmail.com>
2020-03-15 10:34:50 +0530

    Implemented Routes and rendered two components - Chat and Join

---
+++ chat-app/client/src/components/Chat.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chat = ()=>(
+    <h1>Chat page</h1>
+);
+
+export default Chat;
---
+++ chat-app/client/src/components/Join.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Join = ()=>(
+    <h1>Join page</h1>
+);
+
+export default Join;
