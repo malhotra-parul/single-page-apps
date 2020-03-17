707415587227b205f411bcee8d05a9c4a63e7eba
malhotra-parul <parulm.business@gmail.com>
2020-03-17 17:52:17 +0530

    Listening to sendMessage from server and emitting message to server

--- chat-app/client/src/components/Chat/Chat.js
+++ chat-app/client/src/components/Chat/Chat.js
@@ -6,6 +6,8 @@ var socket;
 const Chat = ({location})=>{
     const [name, setName] = useState("");
     const [room, setRoom] = useState("");
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = "localhost:5000"; //server url
 
     useEffect(()=>{
@@ -22,10 +24,31 @@ const Chat = ({location})=>{
             socket.emit("disconnect");
             socket.off();
         }
-    }, [ENDPOINT, location.search])
+    }, [ENDPOINT, location.search]);
+
+    useEffect(()=>{
+        socket.on("message", (message)=>{
+            setMessages([...messages, message]);
+        })
+    }, [messages]);
+
+    const sendMessage = (event)=>{
+        event.preventDefault();
+        if(message){
+            socket.emit("sendMessage", message, ()=> setMessage(""))
+        }
+    };
+
+    console.log(messages, message);
 
     return(
-    <h1>Chat page</h1>
+    <div className="outerContainer">
+        <div className="container">
+            <input value={message}
+                   onChange={(event)=> setMessage(event.target.value)}
+                   onKeyPress={(event)=>event.key === "Enter" ? sendMessage(event) : null}/>
+        </div>
+    </div>
     )
     };
 
--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -13,12 +13,18 @@ io.on("connection", (socket)=>{
     socket.on("join", ({name, room}, callback)=>{
         const {error, user} = addUser({id: socket.id, name, room});
         if(error) return callback(error);
-        socket.emit("message", {user: "admin", msg: `${user.name}, welcome to room ${user.room}!`});
-        socket.broadcast.to(user.room).emit("message", {user:"admin", msg: `${user.name} has joined the chat..`});
+        socket.emit("message", {user: "admin", text: `${user.name}, welcome to room ${user.room}!`});
+        socket.broadcast.to(user.room).emit("message", {user:"admin", text: `${user.name} has joined the chat..`});
 
         socket.join(user.room);
        callback();
     });
+
+    socket.on("sendMessage", (message, callback)=>{
+        const user = getUser(socket.id);
+        io.to(user.room).emit("message", {user: user.name, text: message});
+        callback();
+    })
     socket.on("disconnect", ()=>{
         console.log("User Disconnected!");
     });
