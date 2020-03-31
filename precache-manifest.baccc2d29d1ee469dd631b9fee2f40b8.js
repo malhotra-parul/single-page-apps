f046ad35ad06f8533e454ec6b7271c85e309679f
malhotra-parul <parulm.business@gmail.com>
2020-03-31 19:47:48 +0530

    Updates

---
+++ precache-manifest.baccc2d29d1ee469dd631b9fee2f40b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38970d3350f63b57378e2f8f705d7d92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a216c001417850ed3fe9",
+    "url": "/static/css/main.f04cd514.chunk.css"
+  },
+  {
+    "revision": "121c30858bfcf257f0f1",
+    "url": "/static/js/2.71272214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71272214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a216c001417850ed3fe9",
+    "url": "/static/js/main.e56aed79.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
